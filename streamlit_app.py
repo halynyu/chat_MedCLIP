4141f6b1c34a458314e0a2cd3d01884d64f63a74
하린 <120549760+halynyu@users.noreply.github.com>
2024-05-28 03:13:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,12 @@
-# tmp
-import altair as alt
-import numpy as np
-import pandas as pd
+# 스트림릿 라이브러리를 사용하기 위한 임포트
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+# 웹 대시보드 개발 라이브러리인 스트림릿은,
+# main 함수가 있어야 한다.
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+def main() :
+    st.title('안녕하세요. 웹 대시보드 프로젝트')
+    st.title('Hello')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+if __name__ == '__main__' :
+    main()
